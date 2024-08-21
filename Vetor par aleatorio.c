f22b2960e4500484a4ee9190d70b371c7612f476
chrissnts <160748564+chrissnts@users.noreply.github.com>
2024-08-21 13:58:55 -0300

    Vetor par aleatorio.c

---
+++ Vetor par aleatorio.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 20
+
+int main ()
+{
+   int vet[TAM], vetpar[0];
+   int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < TAM; i++)
+    {
+        vet[i] = 1 + rand()%100;
+
+        if (vet[i] %2 == 0 )
+        {
+            vetpar[i] = vet[i];
+            printf("[%i]", vetpar[i]);
+        }
+    }
+    
+    return 0;
+}
