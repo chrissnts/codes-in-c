8b53a6668608c11052acf0adc6aee0b3b90dac53
chrissnts <160748564+chrissnts@users.noreply.github.com>
2024-08-21 14:54:17 -0300

    Somatorio.c

---
+++ Somatorio.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n = 0, i, k;
+    float soma = 0;
+
+    while (n <= 0)
+    {
+        printf("Digite o valor de n: \n");
+        scanf("%i", &n);
+    }
+
+    i = 1;
+    k = 1;
+
+    while (i <= n)
+    {   
+    
+        soma += (1.0/i*k);
+        k *= -1;
+        i++;
+    }
+    printf("O somatorio eh %f", soma);
+
+    return 0;
+}
