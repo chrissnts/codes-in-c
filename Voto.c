37c90c412809f06f9e63851a615d4c80a085b1a8
chrissnts <160748564+chrissnts@users.noreply.github.com>
2024-08-21 14:15:14 -0300

    Voto.c

---
+++ Voto.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade;
+
+    printf("Digite sua idade: \n");
+    scanf("%i", &idade);
+    if (idade <=0 )
+    {
+         printf("\nIdade inválida");
+        
+    }
+    if( idade >= 16 )
+     {
+        printf("\nPode votar");
+    }else
+    {
+        printf("\nNão pode votar");
+    }
+   
+    return 0;
+}
