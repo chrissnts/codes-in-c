9708df23b75a1f5ef4c0386cfb7623b00cd72551
chrissnts <160748564+chrissnts@users.noreply.github.com>
2024-08-23 12:07:40 -0300

    String diferente.c

--- String diferente.c
+++ String diferente.c
@@ -3,33 +3,67 @@
 #include <time.h>
 
 #define TAM 100
-#define FOR 5
+#define TAM2 1
 
 int main()
 {
-    int i, iguais = 0;
-    int tam;
     char word[TAM], word2[TAM];
-
-    for (i = 0; i < FOR; i++)
+    int tam1, tam2;
+    int i, j, same = 0;
+    
+    
+    printf("\nEnter a word:");
+    gets(word);
+    fflush(stdin);
+    
+    for ( j = 0; j < TAM2; j ++)
     {
-        printf("Escreva primeira palavra: ");
-        gets(word);
-        __fpurge(stdin);
-
-        printf("Escreva segunda palavra: ");
-        gets(word2);
-        __fpurge(stdin);
-
-        if (word[i] == word2[i] )
+         
+        for(tam1 = 0; word [tam1] != '\0'; tam1++)
         {
-            printf("\nIguais.\n");
+            
         }
-        else
+        printf("String 1 tem tamanho [%i] ",tam1);
+        
+        
+        printf("\nEnter a word:");
+        gets(word2);
+        fflush(stdin);
+        
+        for(tam2 = 0; word2[tam2] != '\0'; tam2++)
         {
-            printf("\nDiferentes.\n");
+            
+        }
+        printf("String 2 tem tamanho [%i] ",tam2);
+       
+    
+        if (tam1 != tam2)
+        {
+            printf("\nStrings diferentes.\n");
+        }
+        else 
+        {
+            int aux = 1;
+            
+            for (i = 0; word[i] != '\0'; i++) 
+            {
+                if (word[i] != word2[i]) 
+                {
+                    aux = 0;
+                    break;
+                }
+            }
+        }
+        
+        if (aux == 1)  
+        {
+            printf("\nStrings sao iguais.\n");
+        } 
+        else 
+        {
+            printf("\nStrings sao diferentes.\n");
         }
     }
-
+  
     return 0;
 }
