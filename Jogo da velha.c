7b6694700c4a8205abe8f92b73701a764d9efaa5
chrissnts <christiandossantos125@gmail.com>
2024-08-25 19:44:02 -0300

    25/08/2024

--- Jogo da velha.c
+++ Jogo da velha.c
@@ -5,16 +5,27 @@
 #define JOGADOR_X 'X'
 #define JOGADOR_O 'O'
 #define CARACTER '_'
+#define EMPATE 'E'
 
 int main()
 {
+
+    /*
+        [0] [1] [2]
+    [0]  1   2   3
+    [1]  4   5   6
+    [2]  7   8   9
+
+    */
 
     int lin, col;
     int cont_jogadas = 0;
     int jogada;
     char jogo[TAM][TAM];
+    char jogador_atual = JOGADOR_X;
+    char ganhador = EMPATE;
 
-    // Criando o "menu";
+    // Criando tabuleiro;
     for (lin = 0; lin < TAM; lin++)
     {
         for (col = 0; col < TAM; col++)
@@ -22,109 +33,131 @@ int main()
             jogo[lin][col] = CARACTER;
         }
     }
+    // Laço de jogadas;
+    while (1)
+    {
+        // Apresentando tabuleiro;
 
+        for (lin = 0; lin < TAM; lin++)
+        {
+            for (col = 0; col < TAM; col++)
+            {
+                printf("%c ", jogo[lin][col]);
+            }
+            printf("\n");
+        }
+
+        // Lendo jogada do jogador atual;
+        printf("\nJogador %c, faca sua jogada:\n", jogador_atual);
+        scanf("%i", &jogada);
+
+        // Verificando se jogada é valida;
+        if (!(jogada >= 1 && jogada <= 9))
+        {
+            printf("\nPosicao invalida, digite novamente.\n");
+            continue;
+        }
+
+        // Condicoes de jogadas, jogador x;
+        if (jogada >= 1 && jogada <= 3)
+        {
+            if (jogo[0][jogada - 1] != CARACTER)
+            {
+                printf("\nPosicao ja preenchida, digite novamente.\n");
+                continue;
+            }
+
+            jogo[0][jogada - 1] = jogador_atual;
+        }
+        else if (jogada >= 4 && jogada <= 6)
+        {
+            if (jogo[1][jogada - 4] != CARACTER)
+            {
+                printf("\nPosicao ja preenchida, digite novamente.\n");
+                continue;
+            }
+
+            jogo[1][jogada - 4] = jogador_atual;
+        }
+        else if (jogada >= 7 && jogada <= 9)
+        {
+            if (jogo[2][jogada - 7] != CARACTER)
+            {
+                printf("\nPosicao ja preenchida, digite novamente.\n");
+                continue;
+            }
+
+            jogo[2][jogada - 7] = jogador_atual;
+        }
+
+        // Verifica se as linhas foram preenchidas e jogador X ganhou;
+        for (lin = 0; lin < TAM; lin++)
+        {
+            if (jogo[lin][0] == jogador_atual && jogo[lin][1] == jogador_atual && jogo[lin][2] == jogador_atual)
+            {
+                ganhador = jogador_atual;
+                break;
+            }
+        }
+
+        // Verifica se as colunas foram preenchidas e jogador X ganhou;
+        for (col = 0; col < TAM; col++)
+        {
+            if (jogo[0][col] == jogador_atual && jogo[1][col] == jogador_atual && jogo[2][col] == jogador_atual)
+            {
+                ganhador = jogador_atual;
+                break;
+            }
+        }
+        // Verifica se as diagonais foram preenchidas e jogador X ganhou;
+        if (jogo[0][0] == JOGADOR_X && jogo[1][1] == JOGADOR_X && jogo[2][2] == JOGADOR_X)
+        {
+            ganhador = JOGADOR_X;
+        }
+        else if (jogo[0][2] == JOGADOR_X && jogo[1][1] == JOGADOR_X && jogo[2][0] == JOGADOR_X)
+        {
+            ganhador = JOGADOR_X;
+        }
+
+        // Se houver ganhador ou todas as casas estiverem preenchidas
+        if (ganhador != EMPATE)
+        {
+            break;
+        }
+
+        cont_jogadas++;
+
+        // Verificando se a jogada sera do X ou O;
+        if (jogador_atual == JOGADOR_X)
+        {
+            jogador_atual = JOGADOR_O;
+        }
+        else
+        {
+            jogador_atual = JOGADOR_X;
+        }
+
+        // Se houver ganhador ou todas as casas estiverem preenchidas
+        if (ganhador != EMPATE || cont_jogadas == 9)
+        {
+            break;
+        }
+    }
+    // Apresentando o resultado final;
     for (lin = 0; lin < TAM; lin++)
     {
         for (col = 0; col < TAM; col++)
         {
-            printf("%c", jogo[lin][col]);
+            printf("%c ", jogo[lin][col]);
         }
         printf("\n");
     }
-
-    // Laço de jogadas;
-    while (1)
+    if (ganhador != EMPATE)
     {
-        if (cont_jogadas % 2 == 0)
-        {
-            printf("\nJogador %c, faça sua jogada:\n", JOGADOR_X);
-            scanf("%i", &jogada);
-
-            // Verificando se jogada é valida;
-            if (!(jogada >= 1 && jogada <= 9))
-            {
-                printf("Posicao invalida, digite novamente.\n");
-                continue;
-            }
-
-            // Condicoes de jogadas, jogador A;
-            if (jogada >= 1 && jogada <= 3)
-            {
-                if (jogo[0][jogada - 1] != CARACTER)
-                {
-                    printf("Posicao ja preenchida, digite novamente.");
-                    continue;
-                }
-
-                jogo[0][jogada - 1] = JOGADOR_X;
-            }
-            else if (jogada >= 4 && jogada <= 6)
-            {
-                if (jogo[1][jogada - 4] != CARACTER)
-                {
-                    printf("Posicao ja preenchida, digite novamente.");
-                    continue;
-                }
-
-                jogo[1][jogada - 4] = JOGADOR_X;
-            }
-            else if (jogada >= 7 && jogada <= 9)
-            {
-                 if (jogo[2][jogada - 7] != CARACTER)
-                {
-                    printf("Posicao ja preenchida, digite novamente.");
-                    continue;
-                }
-
-                jogo[2][jogada - 7] = JOGADOR_X;
-            }
-
-            cont_jogadas++;
-        }
-        else 
-        {
-            printf("\nJogador %c, faça sua jogada:\n", JOGADOR_O);
-            scanf("%i", &jogada);
-
-            // Verificando se jogada é valida;
-            if (!(jogada >= 1 && jogada <= 9))
-            {
-                printf("Posicao invalida, digite novamente.\n");
-                continue;
-            }
-
-            // Condicoes de jogadas, jogador A;
-            if (jogada >= 1 && jogada <= 3)
-            {
-                if (jogo[0][jogada - 1] != CARACTER)
-                {
-                    printf("Posicao ja preenchida, digite novamente.");
-                    continue;
-                }
-
-                jogo[0][jogada - 1] = JOGADOR_O;
-            }
-            else if (jogada >= 4 && jogada <= 6)
-            {
-                if (jogo[1][jogada - 4] != CARACTER)
-                {
-                    printf("Posicao ja preenchida, digite novamente.");
-                    continue;
-                }
-
-                jogo[1][jogada - 4] = JOGADOR_O;
-            }
-            else if (jogada >= 7 && jogada <= 9)
-            {
-                if (jogo[2][jogada - 7] != CARACTER)
-                {
-                    printf("Posicao ja preenchida, digite novamente.");
-                    continue;
-                }
-
-                jogo[2][jogada - 7] = JOGADOR_O;
-            }
-        }
-     
+        printf("\nJogador %c venceu!\n", ganhador);
+    }
+    else
+    {
+        printf("\nEmpate!\n");
     }
 }
